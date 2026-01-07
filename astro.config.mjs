92f36d25a86056ae03e38674a6cc599f989d61c2
eramirezl9 <eramirezl9@miumg.edu.gt>
2026-01-07 11:05:47 -0600

    ejorar estilos de botones de proyectos   ┃  para mejor visibilidad y clickeabilidad

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eddyramirez.dev', // Cambiar cuando tengas dominio
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@utils': path.resolve('./src/utils'),
+        '@styles': path.resolve('./src/styles'),
+        '@data': path.resolve('./src/data'),
+        '@content': path.resolve('./src/content')
+      }
+    }
+  }
 });
